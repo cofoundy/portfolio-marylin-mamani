f07bb3124b72c3a4e6d68303b3a5fd5e2013e8ca
Melissa Iman Noriega <melissa.iman.n@uni.pe>
2026-02-18 23:42:32 -0500

    update: custom domain marilynmamani.lat + updated profile photo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cofoundy.github.io',
-  base: '/portfolio-marylin-mamani',
+  site: 'https://marilynmamani.lat',
   vite: {
     plugins: [tailwindcss()],
   },
